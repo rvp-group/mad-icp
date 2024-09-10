815b8564cf52b669b0a4c2a4ba4983dd2e6bff0e
ssg1002 <sgupta2@uni-bonn.de>
2024-09-10 17:22:08 +0200

    use numpy savetxt, open file in append mode

--- mad_icp/apps/utils/utils.py
+++ mad_icp/apps/utils/utils.py
@@ -31,4 +31,4 @@ import numpy as np
 def write_transformed_pose(estimate_file, lidar_to_world, lidar_to_base):
 	base_to_lidar = np.linalg.inv(lidar_to_base)
 	base_to_world = np.dot(lidar_to_base, np.dot(lidar_to_world, base_to_lidar))
-	estimate_file.write(str(base_to_world[:3].reshape(12,))[1:-1] + "\n")
+	np.savetxt(estimate_file, base_to_world[:3].reshape(-1, 12))
